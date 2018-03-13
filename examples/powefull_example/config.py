a0c9627d966e5c53c04e289b442ff4350c746cc0
Ivan Blohin <ivan.blohinn@yandex.ru>
2018-03-13 12:59:54 +0300

    Added powerfull example with JWT, expandable project structure with api version control, custom errors handlers and tests.

---
+++ examples/powefull_example/config.py
@@ -0,0 +1,35 @@
+import os
+import tempfile
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = 'top-secret'
+    JWT_SECRET_KEY = SECRET_KEY
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db_dev.db')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db_test.db')
+
+
+class ProductionConfig(Config):
+    DATABASE_URI = os.environ.get('DATABASE_URI')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    JWT_SECRET_KEY = SECRET_KEY
+
+
+config = {
+    'dev': DevelopmentConfig,
+    'test': TestingConfig,
+    'prod': ProductionConfig
+}
